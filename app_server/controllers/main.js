7030b787e11b245807f83ebf4b43039d895ae8e3
Ivan Arrizabalaga <ivan.arrizabalaga@gmail.com>
2014-07-27 11:07:56 +0200

    Adding some views

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,9 @@
-/*
- * GET home page.
- */
+/* GET 'about us' page */
+module.exports.about = function(req, res){
+	res.render('index', { title: 'About' }); 
+}; 
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+/* GET 'sign in' page */ 
+module.exports.signin = function(req, res){ 
+	res.render('index', { title: 'Sign in' }); 
+}; 
