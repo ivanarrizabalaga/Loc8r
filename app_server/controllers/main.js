a075d2f27feec74386d24fb4b387616c26551966
Ivan Arrizabalaga <ivan.arrizabalaga@gmail.com>
2014-07-30 00:02:30 +0200

    Adds view templates

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,9 @@
 /* GET 'about us' page */
 module.exports.about = function(req, res){
-	res.render('index', { title: 'About' }); 
+	res.render('generic-text', { title: 'About' }); 
 }; 
 
 /* GET 'sign in' page */ 
 module.exports.signin = function(req, res){ 
-	res.render('index', { title: 'Sign in' }); 
+	res.render('signin-index', { title: 'Sign in' }); 
 }; 
